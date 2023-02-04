52a8d6c0f4932a490d32d9ed0274d2bdffad111b
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-02-04 12:58:54 +0200

    get books

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ const db = mysql.createConnection({
 app.get("/" , (req, res) => {
     res.json("hello this is the backend")
 })
+
+app.get("/books" , (req, res) => {
+    const q = "SELECT * FROM books"
+    db.query(q, (err, data)=> {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
 app.listen(8800, () => {
     console.log("Server is Running!")
 })
