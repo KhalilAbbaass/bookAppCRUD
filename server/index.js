a60b64e50646e03988f0891bb10359c5d092a3fd
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-02-04 13:18:13 +0200

    post a book

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const db = mysql.createConnection({
     password:"PFH#23kgrw9",
     database:"bookApp"
 })
+app.use(express.json());
 
 app.get("/" , (req, res) => {
     res.json("hello this is the backend")
@@ -19,6 +20,20 @@ app.get("/books" , (req, res) => {
     db.query(q, (err, data)=> {
         if(err) return res.json(err)
         return res.json(data)
+    })
+})
+
+app.post("/books" , (req, res) => {
+    const q = "INSERT INTO books (`title` , `desc` , `cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover
+    ]
+
+    db.query(q, [values], (err,data) => {
+        if(err) return res.json(err)
+        return res.json("book created successfully")
     })
 })
 app.listen(8800, () => {
