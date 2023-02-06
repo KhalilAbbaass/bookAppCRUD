595d2868370e98f83876797dec9bb3ed7b57f7b8
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-02-06 11:27:35 +0200

    client side routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Add from "./pages/Add";
+import Books from "./pages/Books";
+import Update from "./pages/Update";
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (<Books/>),
+    },
+    {
+      path: "/add",
+      element: (<Add/>),
+    },
+    {
+      path: "/update",
+      element: (<Update/>),
+    },
+  ]);
   return (
     <div className="App">
-      
+       <RouterProvider router={router} />
     </div>
   );
 }
