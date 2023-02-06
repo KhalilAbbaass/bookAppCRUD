36e3b85b4ce9ec4d5f2637d39914732ee4f4129f
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-02-06 12:37:07 +0200

    update price in query

--- server/index.js
+++ server/index.js
@@ -26,11 +26,12 @@ app.get("/books" , (req, res) => {
 })
 
 app.post("/books" , (req, res) => {
-    const q = "INSERT INTO books (`title` , `desc` , `cover`) VALUES (?)"
+    const q = "INSERT INTO books (`title` , `desc` , `cover`, `price`) VALUES (?)"
     const values = [
         req.body.title,
         req.body.desc,
-        req.body.cover
+        req.body.cover,
+        req.body.price
     ]
 
     db.query(q, [values], (err,data) => {
